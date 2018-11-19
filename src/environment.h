b7bcd3f5adbcce29dd884810cb520a6dc75170d9
chris harn <chris.harn@gmail.com>
2018-11-18 21:43:14 -0800

    Filled out common variables to environment

--- src/environment.h
+++ src/environment.h
@@ -1,8 +1,14 @@
 #ifndef __ENVIRONMENT__
 #define __ENVIRONMENT__
 
-struct environment {
-	double altitude;  /* in feet */
+struct environment {		/* UNITS - NAME */
+	double m_altitude;	/* feet - Altitude */
+	double m_time;		/* milliseconds - Current Time from Start of Sim */
+	double m_gravity;	/* m/s^2 at a particular altitude */
+	double m_rho;		/* kg/m^3 - Air Density */
+	double m_dynamicPressure; /* Pa - Dynamic Pressure */
+	double m_mach;		/* mach - Mach Number */
+	double m_speedOfSound;  /* m/s - Speed of Sound */
 };
 
 
